edb46388ed05497795b554002cd39ed4d685c16d
Aldokler <xdavidpastor@gmail.com>
2024-03-11 20:33:06 -0600

    login001

--- Frontend/GJ-Platform/src/app/login/login.component.ts
+++ Frontend/GJ-Platform/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  errorMessage: string = "";
+  constructor(private router: Router) { }
 
-  evaluarUsuario(correoUsuario: string, contrasenaInput: string): void {}
-  verContrasena(): void {}
+  //Esta función es para evitar que se recargue la página al hacer el submit
+  handleSubmit(event: Event): void {
+    event.preventDefault(); // Evita la acción predeterminada del formulario (recargar la página)
+    const emailInput = (event.target as HTMLFormElement).querySelector('#emailInput') as HTMLInputElement;
+    const email = emailInput.value;
+    this.sendEmail(email);
+  }
+
+  sendEmail(email: String){
+    //Do something!
+    console.log("Enviando link de inicio de sesión a: ", email);
+  }
+  discordLogin(){
+    //Do something!
+    console.log("Aquí  Paublo hace magia o algo");
+  }
 }
