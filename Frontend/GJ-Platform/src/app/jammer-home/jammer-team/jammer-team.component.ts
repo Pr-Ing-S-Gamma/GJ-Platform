1752f64b80819dc04313768f83865f96a66888c6
yuuut4 <159486768+yuuut4@users.noreply.github.com>
2024-04-20 03:10:21 -0600

    GJP-52 Jammer - Add member
    Se añade la funcionalidad para añadir miembros a un equipo

--- Frontend/GJ-Platform/src/app/jammer-home/jammer-team/jammer-team.component.ts
+++ Frontend/GJ-Platform/src/app/jammer-home/jammer-team/jammer-team.component.ts
@@ -1,27 +1,106 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-jammer-team',
+  templateUrl: './jammer-team.component.html',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
   ],
-  templateUrl: './jammer-team.component.html',
-  styleUrl: './jammer-team.component.css'
+  styleUrls: ['./jammer-team.component.css']
 })
 export class JammerTeamComponent {
 
   members = [
-    {id:1, name:'David Pastor Barrientos', discord:'Aldokler', email:'xdavidpastor@gmail.com'},
-    {id:2, name:'Atlas09 Herrera', discord:'Adrian', email:'maenomesesucorreo@gmail.com'},
-  ]
+    { id: 1, name: 'David Pastor Barrientos', discord: 'Aldokler', email: 'xdavidpastor@gmail.com' },
+    { id: 2, name: 'Atlas09 Herrera', discord: 'Adrian', email: 'maenomesesucorreo@gmail.com' },
+  ];
+  possibleMembers: any[] = [
+    { id: 3, name: 'Pedro Pascal', discord: 'pedritpo', email: 'pedritpo@gmail.com' },
+    { id: 4, name: 'Miguel Ángel', discord: 'migue123', email: 'migue23@gmail.com' },
+  ];
+  
+  showAddMemberModal: boolean = false;
+  suggestionsVisible: boolean = false;
+  isTriangleUp: boolean = true; 
+  newMember: any = {};
+  nameSuggestions: any[] = [];
+  memberNameSuggestions: any[] = []; 
+  filteredSuggestions: any[] = [];
 
-  toggleAddMember(){
-    //Do something!
+  toggleSuggestionsVisibility() {
+    this.suggestionsVisible = !this.suggestionsVisible;
+    this.toggleTriangleDirection();
   }
 
-  leaveTeam(){
-    //Do something!
+  toggleTriangleDirection() {
+    this.isTriangleUp = !this.isTriangleUp;
+  }
+
+  toggleAddMemberModal() {
+    this.showAddMemberModal = !this.showAddMemberModal;
+    if (!this.showAddMemberModal) {
+      this.clearForm();
+    }
+    this.filteredSuggestions = [];
+  }
+  
+
+
+  suggestNames(event: Event) {
+    const searchTerm = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.suggestionsVisible = true; 
+  
+    if (searchTerm === '') {
+      this.filteredSuggestions = [...this.possibleMembers];
+      this.clearNewMember(); 
+    } else {
+      this.filteredSuggestions = this.possibleMembers.filter(member =>
+        member.name.toLowerCase().startsWith(searchTerm)
+      );
+    }
+  }
+  
+
+  
+  
+clearInputOnBackspace(event: KeyboardEvent) {
+    if (event.key === 'Backspace' || event.key === 'Delete') {
+      this.newMember.email = "";
+      this.newMember.discord = "";
+    }
+  }
+  
+  clearNewMember() {
+    this.newMember = {};
+  }
+
+  selectSuggestion(selectedMember: any) {
+    this.newMember.name = selectedMember.name;
+    this.newMember.email = selectedMember.email;
+    this.newMember.discord = selectedMember.discord;
+    this.filteredSuggestions = [];
+  }
+  
+
+  addMember() {
+    if (this.newMember.email && this.newMember.name && this.newMember.discord) {
+      const newMemberCopy = { ...this.newMember };
+      this.members.push(newMemberCopy);
+      this.memberNameSuggestions.push(newMemberCopy.name);
+      this.clearForm();
+    }
+  }
+
+  clearForm() {
+    this.newMember = {};
+    this.filteredSuggestions = []; 
+  }
+
+  leaveTeam() {
+
   }
 }
