2aafc36425ecd465eb76d18bfb103cdbae91d37c
Jefry99XD <jefrycc99@gmail.com>
2024-04-23 02:39:10 -0600

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -15,25 +15,25 @@ const port = 3000; // Establecer el puerto en el que el servidor escuchará las 
 mongoose.connect("mongodb://localhost:27017/GameJamDB");
 
 // Configuración de CORS - Permite solicitudes desde un origen específico
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (!origin) return callback(null, true);
+// const corsOptions = {
+//     origin: function(origin, callback) {
+//         if (!origin) return callback(null, true);
 
-        const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200', 'http://149.130.176.112','http://149.130.176.112:3000'];
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            // El origen está en la lista de orígenes permitidos
-            callback(null, true);
-        } else {
-            // El origen no está en la lista de orígenes permitidos
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 204, // Devolver un código de éxito 204
-    methods: "GET, POST, PUT, DELETE", // Permitir estos métodos HTTP
-    credentials: true, // Permite enviar cookies de forma segura
-};
+//         const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200', 'http://149.130.176.112','http://149.130.176.112:3000'];
+//         if (allowedOrigins.indexOf(origin) !== -1) {
+//             // El origen está en la lista de orígenes permitidos
+//             callback(null, true);
+//         } else {
+//             // El origen no está en la lista de orígenes permitidos
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     optionsSuccessStatus: 204, // Devolver un código de éxito 204
+//     methods: "GET, POST, PUT, DELETE", // Permitir estos métodos HTTP
+//     credentials: true, // Permite enviar cookies de forma segura
+// };
 
-app.use(cors(corsOptions)); // Usar el middleware CORS
+//app.use(cors(corsOptions)); // Usar el middleware CORS
 
 // Middleware para analizar solicitudes JSON y cookies
 app.use(express.json());
