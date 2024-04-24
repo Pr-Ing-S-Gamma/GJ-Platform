7af10cbb02de421dc6d43d6176d476b55ca999c2
yuuut4 <159486768+yuuut4@users.noreply.github.com>
2024-04-23 19:25:14 -0600

    Revert "."
    This reverts commit 44792b7ae5bbad2e2dc7d44d5c031407806d623d.

--- Frontend/GJ-Platform/src/app/jammer-home/jammer-home.component.ts
+++ Frontend/GJ-Platform/src/app/jammer-home/jammer-home.component.ts
@@ -1,53 +1,15 @@
-import { CommonModule, NgClass, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-jammer-home',
   standalone: true,
   imports: [
-    NgClass,
-    NgIf,
     CommonModule
   ],
   templateUrl: './jammer-home.component.html',
   styleUrl: './jammer-home.component.css'
 })
 export class JammerHomeComponent {
-  showTeam: boolean = false;
-  constructor(private router: Router, private userService: UserService) { }
-
-  moveToSites() {
-    this.router.navigate(['/Sites']);
-  }
-
-  private hide(){
-    this.showTeam = false;
-
-  }
-
-  toggleTeam() {
-    this.hide();
-    this.showTeam = true;
-  }
-
-
-  logOut(): void {
-    this.userService.logOutUser('http://localhost:3000/api/user/log-out-user')
-      .subscribe(
-        () => {
-          this.router.navigate(['/login']);
-        },
-        error => {
-          console.error('Error al cerrar sesión:', error);
-        }
-      );
-  }
-
-  a(){
-    console.log("aa")
-  }
-
 
 }
