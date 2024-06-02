59c21c667c328c7b5e9a0cecbf8c8dc452381299
Jefry99XD <jefrycc99@gmail.com>
2024-06-01 22:39:36 -0600

    6

--- Frontend/GJ-Platform/src/app/game-information/game-information.component.ts
+++ Frontend/GJ-Platform/src/app/game-information/game-information.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { Category, Team, Theme } from '../../types';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SubmissionService } from '../services/submission.service';
 import { RateFormComponent } from './rate-form/rate-form.component';
-import { Submission } from '../../types';
+import { Submission, Team, Theme, Category } from '../../types';
 import { TeamService } from '../services/team.service';
 import { ThemeService } from '../services/theme.service';
 import { CategoryService } from '../services/category.service';
@@ -30,7 +28,16 @@ export class GameInformationComponent implements OnInit {
   gameParameter!: string;
   ActualUserIsJuez: Boolean = false;
   evaluando: Boolean = false;
-  dataSource: any = null;
+  dataSource: any = {
+    name: '',
+    team: '',
+    description: '',
+    teamMembers: [],
+    themes: [],
+    categories: [],
+    gameLink: '',
+    pitchLink: ''
+  };
   gameTitle: string = '';
   teamName: string = '';
   gameDescription: string = '';
@@ -60,18 +67,19 @@ export class GameInformationComponent implements OnInit {
             this.ActualUserIsJuez = true;
           }
         },
-        () => {
+        error => {
+          console.error('Error al obtener el usuario:', error);
         }
       );
       this.gameParameter = this.game;
-      var url = `http://${environment.apiUrl}:3000/api/submission/get-submission/` + this.game;
+      const url = `http://${environment.apiUrl}:3000/api/submission/get-submission/${this.game}`;
       this.SubmissionService.getSubmission(url).subscribe(
         (game: Submission) => {
           this.gameLink = game.game;
           this.pitchLink = game.pitch;
           this.gameTitle = game.title;
           this.gameDescription = game.description;
-          const urlj = `http://${environment.apiUrl}:3000/api/team/get-team/` + game.teamId
+          const urlj = `http://${environment.apiUrl}:3000/api/team/get-team/${game.teamId}`;
           this.TeamService.getTeamById(urlj).subscribe(
             (team: Team) => {
               this.teamName = team.studioName;
@@ -80,12 +88,12 @@ export class GameInformationComponent implements OnInit {
                 name: jammer.name,
                 discordUsername: jammer.discordUsername,
                 email: jammer.email
-            }));
-              const urlc = `http://${environment.apiUrl}:3000/api/category/get-category/` + game.categoryId
+              }));
+              const urlc = `http://${environment.apiUrl}:3000/api/category/get-category/${game.categoryId}`;
               this.CategoryService.getCategory(urlc).subscribe(
-                (categories: Category) => {
-                  this.categories = [categories.titleEN];
-                  const urlt = `http://${environment.apiUrl}:3000/api/theme/get-theme/` + game.themeId
+                (category: Category) => {
+                  this.categories = [category.titleEN];
+                  const urlt = `http://${environment.apiUrl}:3000/api/theme/get-theme/${game.themeId}`;
                   this.ThemeService.getTheme(urlt).subscribe(
                     (theme: Theme) => { 
                       this.themes = theme.titleEN !== undefined ? [theme.titleEN] : [];
@@ -98,27 +106,27 @@ export class GameInformationComponent implements OnInit {
                         categories: this.categories,
                         gameLink: this.gameLink,
                         pitchLink: this.pitchLink
-                      }
+                      };
                     },
                     error => {
-                      console.error('Error al obtener juegos:', error);
+                      console.error('Error al obtener el tema:', error);
                     }
-                  )
+                  );
                 },
                 error => {
-                  console.error('Error al obtener juegos:', error);
+                  console.error('Error al obtener la categoría:', error);
                 }
-              )
+              );
             },
             error => {
-              console.error('Error al obtener juegos:', error);
+              console.error('Error al obtener el equipo:', error);
             }
-          )
+          );
         },
         error => {
-          console.error('Error al obtener juegos:', error);
+          console.error('Error al obtener la entrega:', error);
         }
-      )
+      );
     });
   }  
 }
