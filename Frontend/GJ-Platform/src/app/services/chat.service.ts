743048ac4b84298380597fd0a3ec637fd4163511
SharonChR <sharonnathalia21@gmail.com>
2024-06-06 00:22:58 -0600

    chat

--- Frontend/GJ-Platform/src/app/services/chat.service.ts
+++ Frontend/GJ-Platform/src/app/services/chat.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Chat } from '../../types';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment.prod';
+
 
 
 
@@ -7,7 +13,18 @@ import { Injectable } from '@angular/core';
 })
 export class ChatService {
 
-  constructor() {
+  constructor(private http: HttpClient) {}
 
-  }
+    private baseUrl = `http://${environment.apiUrl}:3000/api/chat/`;
+
+
+    createChat(chat: Chat): Observable<any> {
+      return this.http.post(this.baseUrl+'create-chat', chat, { withCredentials: true });
+    }
+
+    getChat(chat: Chat, id: string): Observable<any> {
+      return this.http.get<any>(this.baseUrl+`get-chat/:${id}`, { withCredentials: true }).pipe(
+        map(response => response.data))
+    }
+  
 }
